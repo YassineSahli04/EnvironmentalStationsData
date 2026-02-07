53178728b6e7402ff7edde5879dbe09766042a59
Hedi Bouassida <hbouassida.esct@gmail.com>
2026-02-06 19:22:49 -0600

    first itaration of nasa station

---
+++ BackEnd/NasaStations/NasaStationsApiCalls.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+
+
+class NasaPowerApiCalls:
+    # NASA Base Endpoint
+    BASE_URL = "https://power.larc.nasa.gov/api/temporal/Hourly/point"
+    def __init__(self, lat, lon, start_date, end_date):
+        self.lat = lat
+        self.lon = lon
+        self.start = start_date # Format: YYYYMMDDHH
+        self.end = end_date     # Format: YYYYMMDDHH
+        
+        # We don't need a list[QueryObject] because NASA doesn't support 
+        # complex batching of different stations in one call.
+        # One Object = One Station Request.
+    def get_params(self):
+        """
+        Instead of getBody(), we have get_params() to build the query string.
+        """
+        return {
+            "parameters": "T2M,RH2M,PRECTOTCORR,ALLSKY_SFC_SW_DWN", 
+            "community": "AG", # AG = Agrometeorology
+            "longitude": self.lon,
+            "latitude": self.lat,
+            "start": self.start,
+            "end": self.end,
+            "format": "JSON"
+        }
+    def get_response(self):
+        # The 'params' argument automatically encodes the dictionary 
+        # into the URL (e.g. ?longitude=10&latitude=34...)
+        response = requests.get(
+            self.BASE_URL, 
+            params=self.get_params(),
+            timeout=10
+        )
+        response.raise_for_status() # Crash immediately if NASA is down (404/500)
+        return response.json()S
