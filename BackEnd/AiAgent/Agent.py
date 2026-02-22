84b3b1eee025ec1e4ec6a314f1c8c54de1626ce5
Yassine SAHLI <ysahl091@uottawa.ca>
2026-02-22 16:14:59 -0500

    created an agent class

---
+++ BackEnd/AiAgent/Agent.py
@@ -0,0 +1,42 @@
+from langchain_ollama import ChatOllama
+from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.runnables import RunnableConfig
+
+def Agent():
+    def __init__(self):
+        checkpointer = InMemorySaver()
+
+        llm = ChatOllama(
+            model="phi3.5",
+            base_url="http://100.67.117.80:11434",
+            temperature=0.2
+            )
+
+        self.agent = create_agent(
+            model=llm,
+            checkpointer=checkpointer
+            )
+
+        config: RunnableConfig = {"configurable": {"thread_id": "1"}}
+        self.config = config
+
+        self.agent.invoke(
+            {"messages": [SystemMessage(content="You are a helpful assistant. All your answers will need to be short and concise.")]},
+            config= config
+        )
+
+    def initializeChat(self):
+        while(True):
+            userMessage = input("User: ").strip()
+            if userMessage.lower() == "exit":
+                break
+
+            print("Bot: ", end="", flush=True)
+            response = self.agent.invoke(
+                    {"messages":[HumanMessage(content=userMessage)]},
+                    config=self.config
+                )
+            print(response["messages"][-1].content, end="", flush=True)  # type: ignore
+            print()
